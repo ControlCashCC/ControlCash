cde669260877ec035706cc8e295359b6bea33380
unknown <gabriel_paixao2015@hotmail.com>
2025-08-26 17:19:59 -0300

    [INITIAL] Defining and creating the project structure

---
+++ front-end/utils/importCss.js
@@ -0,0 +1,7 @@
+// Função para exportar o arquivo CSS
+export function importStyle(path) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = path;
+    document.head.appendChild(link);
+}
